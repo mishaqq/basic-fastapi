e6abcf8476fc575b74a86b262444d01ca3be1fed
Mykhailo Danylenko <dron7432@gmail.com>
2025-05-13 17:40:41 +0200

    auth with JWT token v1.0 bad architecture

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False, "timeout": 15}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from pydantic import BaseModel, EmailStr
 from typing import List, Annotated
 import models
 from database import SessionLocal, engine
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta, timezone
 
+import jwt
+from jwt.exceptions import InvalidTokenError
+SECRET = "692231733f2092790aff8c5aa3a874480775d5c241cf09de0a876344350abf70"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     
 
 models.Base.metadata.create_all(bind=engine)
@@ -26,7 +33,6 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 
 class User(BaseModel):
-    id: str
     username: str
     email: EmailStr | None = None
 
@@ -36,9 +42,21 @@ class UserInDB(User):
 class UserIn(User):
     password: str
 
+def veryfy_password(password, hashed_password):
+        return pwd_context.verify(password, hashed_password)
 
+def get_password_hash(password):
+        return pwd_context.hash(password)
+async def authenticate_user(username: str, password: str, db: Session):
+        user = db.query(models.User).filter(models.User.username == username).first()
+        if not user: 
+                return None
+        if not veryfy_password(password, user.hashed_password):
+                return None
+        return UserInDB(id=user.id, username=user.username, email=user.email, hashed_password=user.hashed_password)
+        
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: db_dependency) -> User:
-        user = await fake_decode_token(token, db)
+        user = await decode_token(token, db)
         if not user:
                 raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
@@ -46,36 +64,62 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: db
                 headers={"WWW-Authenticate": "Bearer"},
                 )
         return user
-    
-async def fake_decode_token(token: str, db: Session) -> User | None:
-        user_from_db = db.query(models.User).filter(models.User.username == token).first()
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+        to_encode = data.copy()
+        if expires_delta:
+                expire = datetime.utcnow() + expires_delta
+        else:
+                expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET, algorithm=ALGORITHM)
+        return encoded_jwt
+
+
+async def decode_token(token: str, db: Session) -> User | None:
+        credentials_exception = HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+                )   
+        try:
+               payload = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
+               username = payload.get("sub")
+              
+               if username is None:
+                       raise credentials_exception
+        except InvalidTokenError:
+                raise credentials_exception       
+        user_from_db = db.query(models.User).filter(models.User.username == username).first()
         if not user_from_db:
                 return None
-        return User(id=user_from_db.id, username=user_from_db.username, email=user_from_db.email)
+        return User( username=user_from_db.username, email=user_from_db.email)
 
 @app.get("/")
 async def helloWorld(token: Annotated[str, Depends(oauth2_scheme)]):
         return {"token": token}
 
+#db.query(models.User).filter(models.User.username == form_data.username).first()
 
 @app.post('/token')
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
-        user_dict = db.query(models.User).filter(models.User.username == form_data.username).first()
-        if not user_dict:
+        user = await authenticate_user(form_data.username, form_data.password, db)
+        if not user:
                raise HTTPException(status_code=400, detail="Incorrect username or password")
-        user_db = UserInDB(id=user_dict.id, username=user_dict.username, email=user_dict.email, hashed_password=user_dict.hashed_password)
-        hashed_password = user_db.hashed_password
-        if hashed_password != form_data.password:
-               raise HTTPException(status_code=400, detail="Incorrect username or password")
-        return {"access_token": user_db.username, "token_type": "bearer"}
+        user_db = UserInDB( username=user.username, email=user.email, hashed_password=user.hashed_password)
+        access_token = create_access_token(data= {"sub" : user.username}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+
+        
+        return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.get('/user/me', response_model=User)
 async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
        return current_user
+
+
 @app.post("/fake_register")
 async def fake_register(user: UserIn, db: db_dependency):
-        db_User = models.User(username=user.username, hashed_password=user.password)
+        db_User = models.User(username=user.username, hashed_password=get_password_hash(user.password), email=user.email)
         db.add(db_User)
         db.commit()
 
